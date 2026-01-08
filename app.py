3b183c76c5ad4e94e1ca8125df4f359abdd557b9
Rafael Coelho <rafaelcss3@hotmail.com>
2026-01-08 15:43:41 -0300

    Atualização para Render: ajuste app.run e requirements.txt

--- app.py
+++ app.py
@@ -241,5 +241,8 @@ def editar_epc(id):
 
 
 # ------------------ Start Flask ------------------
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
