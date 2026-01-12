b7aaabb225fb6e230890833025de15e7a6b7d558
Rafael Coelho <rafaelcss3@hotmail.com>
2026-01-12 14:56:58 -0300

    Ajusta conexão MySQL para variáveis de ambiente

--- db.py
+++ db.py
@@ -3,12 +3,16 @@ import os
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.environ.get("localhost"),
-        user=os.environ.get("root"),
-        password=os.environ.get("1686074670"),
-        database=os.environ.get("controle"),
-        port=int(os.environ.get("DB_PORT", 3306))
+        host=os.environ.get("MYSQL_HOST", "localhost"),
+        user=os.environ.get("MYSQL_USER", "root"),
+        password=os.environ.get("MYSQL_PASSWORD", "1686074670"),
+        database=os.environ.get("MYSQL_DATABASE", "controle"),
+        port=int(os.environ.get("MYSQL_PORT", 3306))
     )
+
+
+
+
 
 
 
