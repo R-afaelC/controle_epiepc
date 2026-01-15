a562379843523b79171d538e575b0f6b7438bb74
Rafael Coelho <rafaelcss3@hotmail.com>
2026-01-15 17:01:02 -0300

    Corrige conexão com MySQL para Railway

--- db.py
+++ db.py
@@ -1,17 +1,14 @@
 import mysql.connector
 import os
-
 def get_connection():
-    return mysql.connector.connect(
-        host=os.environ.get("MYSQL_HOST", "containers-us-west-xxx.railway.app"),
-        user=os.environ.get("USUARIO_MYSQL", "root"),
-        password=os.environ.get("SENHA_DO_MYSQL", "1686074670"),
-        database=os.environ.get("BANCO_DE_DADOS_MYSQL", "controle"),
-        port=int(os.environ.get("MYSQL_PORT", 3306)),
-        connection_timeout=5
-
-    )
-
+     return mysql.connector.connect( 
+            host=os.environ["MYSQL_HOST"],
+            user=os.environ["USUARIO_MYSQL"],
+            password=os.environ["MSQL_PASSWORD"],
+            database=os.environ["BANCO_DE_DADOS_MYSQL"],
+            port=int(os.environ["MYSQL_PORT"]),
+             connection_timeout=5
+              )
 
 
 
