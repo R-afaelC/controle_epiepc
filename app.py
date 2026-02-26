141579c6f5f1123232b741cc4d61ec599c0fc6d1
Rafael Coelho <rafaelcss3@hotmail.com>
2026-02-26 14:28:45 -0300

    Adicionando estrutura PostgreSQL e tabelas EPI/EPC

--- app.py
+++ app.py
@@ -260,9 +260,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
-    def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+
