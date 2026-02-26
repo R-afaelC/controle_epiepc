9e3592733fc23000deb1ba49fe2b4a0db822ad0d
Rafael Coelho <rafaelcss3@hotmail.com>
2026-02-26 14:01:38 -0300

    Adicionando estrutura PostgreSQL e tabelas EPI/EPC

--- db.py
+++ db.py
@@ -1,1 +1,12 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Corrige caso venha como postgres://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
